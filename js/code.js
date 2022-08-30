064d66ad0e6b3f0e2cfda77f3b8ba1798c6b17a1
VinnyAlmeidaBR <110014684+VinnyAlmeidaBR@users.noreply.github.com>
2022-08-30 11:53:15 -0300

    release-2.5

--- js/code.js
+++ js/code.js
@@ -1,1 +1,33 @@
 //Codigos para o hotsite Rubem Braga//
+
+//CRIA UM NOVO OBJETO DA CLASSE "DATE"//
+const tempo = new Date();
+// console.log(tempo.getFullYear() + '/' +  tempo.getHours() + '/' + tempo.getMinutes());
+const txtData = tempo.getFullYear() + '/' + tempo.getHours() + '/' + tempo.getMinutes();
+
+//elementos dom 
+const spanData = document.getElementById('data')
+const btFechar = document.getElementById('btFechar')
+const divModal = document.getElementById('divModal')
+const btHamburguer = document.getElementById('btHamburguer')
+const navPhone = document.getElementById('navPhone')
+const btFecharMenu = document.getElementById('btFecharMenu')
+
+//Insere o txtData dentro do ID "data"
+
+spanData.innerText = txtData;
+
+//ao clicar no id "btFechar"
+
+btFechar.addEventListener('click', function () {
+    //oculta o modal*
+    divModal.style.display = 'none';
+})
+
+btHamburguer.addEventListener('click', function () {
+    navPhone.style.display = 'block';
+})
+
+btFecharMenu.addEventListener('click', function (){
+    navPhone.style.display = 'none';
+})
